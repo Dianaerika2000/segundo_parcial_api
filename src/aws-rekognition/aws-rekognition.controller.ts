54d6196e11579355b820cd6d5fd6db8c8694233c
dianaM <diana.montano@assuresoftbolivia.com>
2023-11-27 01:07:21 -0400

    Add aws rekognition module

--- src/aws-rekognition/aws-rekognition.controller.ts
+++ src/aws-rekognition/aws-rekognition.controller.ts
@@ -1,34 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller} from '@nestjs/common';
 import { AwsRekognitionService } from './aws-rekognition.service';
-import { CreateAwsRekognitionDto } from './dto/create-aws-rekognition.dto';
-import { UpdateAwsRekognitionDto } from './dto/update-aws-rekognition.dto';
 
 @Controller('aws-rekognition')
 export class AwsRekognitionController {
   constructor(private readonly awsRekognitionService: AwsRekognitionService) {}
-
-  @Post()
-  create(@Body() createAwsRekognitionDto: CreateAwsRekognitionDto) {
-    return this.awsRekognitionService.create(createAwsRekognitionDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.awsRekognitionService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.awsRekognitionService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAwsRekognitionDto: UpdateAwsRekognitionDto) {
-    return this.awsRekognitionService.update(+id, updateAwsRekognitionDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.awsRekognitionService.remove(+id);
-  }
 }
--- src/aws-rekognition/aws-rekognition.service.ts
+++ src/aws-rekognition/aws-rekognition.service.ts
@@ -1,26 +1,178 @@
 import { Injectable } from '@nestjs/common';
-import { CreateAwsRekognitionDto } from './dto/create-aws-rekognition.dto';
-import { UpdateAwsRekognitionDto } from './dto/update-aws-rekognition.dto';
+import { ConfigService } from '@nestjs/config';
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3';
+import { CreateCollectionCommand, IndexFacesCommand, RekognitionClient, SearchFacesByImageCommand, SearchFacesByImageCommandOutput } from '@aws-sdk/client-rekognition';
 
 @Injectable()
 export class AwsRekognitionService {
-  create(createAwsRekognitionDto: CreateAwsRekognitionDto) {
-    return 'This action adds a new awsRekognition';
+  constructor(
+    private readonly configService: ConfigService,
+  ) {}
+
+  async createCollection(collectionName: string) {
+    const rekognitionClient = new RekognitionClient({
+      region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+
+    try {
+      const data = await rekognitionClient.send(
+        new CreateCollectionCommand({ CollectionId: collectionName }),
+      );
+
+      return data;
+    } catch (error) {
+      console.log('Error', error.stack);
+    }
   }
 
-  findAll() {
-    return `This action returns all awsRekognition`;
+  async indexFaceToCollection(profilePhoto: Buffer, collectionName: string) {
+    // Step 1: Connects to AWS Rekognition service
+    const rekognitionClient = new RekognitionClient({
+      region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+  
+    // Index the face in the collection
+    const insertInCollectionResult = await rekognitionClient.send(
+      new IndexFacesCommand({
+        CollectionId: collectionName,
+        Image: {
+          Bytes: profilePhoto,
+        },
+        MaxFaces: 1,
+        QualityFilter: 'MEDIUM',
+      }),
+    );
+
+    const faceId = insertInCollectionResult?.FaceRecords[0]?.Face?.FaceId;
+    const successfullyInsertedInCollection = insertInCollectionResult.$metadata.httpStatusCode === 200 && faceId;
+    
+    // Step 2: Call the `uploadProfilePhotoToS3()` function to upload the user's image to S3
+    const { profilePhotoUrl } = await this.uploadProfilePhotoToS3(profilePhoto, faceId);
+    
+    if (successfullyInsertedInCollection) {
+      return {
+        message: 'Successfully indexed the face in the collection.',
+        faceId,
+        img_url: profilePhotoUrl
+      };
+    } else {
+      return {
+        error: 'Something went wrong while indexing the face in the collection.',
+      };
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} awsRekognition`;
+  async uploadProfilePhotoToS3( profilePhotoBuffer: Buffer, faceId: string) {
+    const s3Bucket = this.configService.get('AWS_S3_BUCKET');
+
+    // Step 1: Connects to AWS S3 service
+    const s3Client = new S3Client({
+      region: this.configService.get('AWS_REGION_S3'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+
+    // Step 2: Create an S3 object with the user's image
+    const s3Object = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: s3Bucket,
+        Key: `user_avatar_${faceId}.jpeg`,
+        Body: profilePhotoBuffer,
+        ContentType: 'image/jpeg',
+      }),
+    );
+    
+    const s3ObjectUrl = `https://${s3Bucket}.s3.amazonaws.com/user_avatar_${faceId}.jpeg`;
+  
+    return {
+      profilePhotoUrl: s3ObjectUrl,
+    };
   }
 
-  update(id: number, updateAwsRekognitionDto: UpdateAwsRekognitionDto) {
-    return `This action updates a #${id} awsRekognition`;
+  async getUsersByPhotography(photography: Buffer, collectionName: string) {
+
+    // Connects to AWS Rekognition service
+    const rekognitionClient = new RekognitionClient({
+      region: this.configService.get('AWS_REGION'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      }
+    });
+
+    // Search faces in our collection using the main face from the image (if it exist)
+    const searchFacesResult: SearchFacesByImageCommandOutput = await rekognitionClient.send(new SearchFacesByImageCommand({
+      CollectionId: collectionName,
+      Image: {
+        Bytes: photography
+      },
+      MaxFaces: 20,
+      QualityFilter: 'MEDIUM'
+    }))
+
+    const faceIds = this.getFaceIdsFromSearchFacesResult(searchFacesResult)
+
+    const found = searchFacesResult?.$metadata?.httpStatusCode === 200 && faceIds.length > 0
+
+    if (found) {
+      return {
+        message: 'Successfully found users by photography.',
+        users: faceIds
+      }
+    } else {
+      return {
+        error: "Couldn't find users by photo."
+      }
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} awsRekognition`;
+  private getFaceIdsFromSearchFacesResult(searchFacesResult: SearchFacesByImageCommandOutput) {
+    return searchFacesResult.FaceMatches.map((faceMatch) => {
+      return {
+        faceId: faceMatch.Face.FaceId,
+        similarity: faceMatch.Similarity,
+      };
+    });
+  }
+
+  async uploadPhotography(photography: Buffer, photographerId: string) {
+    const s3Bucket = this.configService.get('AWS_S3_BUCKET');
+
+    // Step 1: Connects to AWS S3 service
+    const s3Client = new S3Client({
+      region: this.configService.get('AWS_REGION_S3'),
+      credentials: {
+        accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+        secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      },
+    });
+
+    // Step 2: Create an S3 object
+    const s3Object = await s3Client.send(
+      new PutObjectCommand({
+        Bucket: s3Bucket,
+        Key: `img_${photographerId}.jpeg`,
+        Body: photography,
+        ContentType: 'image/jpeg',
+      }),
+    );
+
+    console.log("s3Object", s3Object);
+    
+    const s3ObjectUrl = `https://${s3Bucket}.s3.amazonaws.com/img_${photographerId}.jpeg`;
+  
+    return {
+      profilePhotoUrl: s3ObjectUrl,
+    };
   }
 }
